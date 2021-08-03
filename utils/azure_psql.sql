93a3d206292972dc9960b3d72ae59c02dd2607df
Mark Redford <markredford@uvic.ca>
2021-08-02 17:55:41 -0700

    adding schema for Azure PostgreSQL database

---
+++ utils/azure_psql.sql
@@ -0,0 +1,27 @@
+-- Table: public.overlays
+
+-- DROP TABLE public.overlays;
+
+CREATE TABLE public.overlays
+(
+    created timestamp without time zone,
+    creator character varying(25) COLLATE pg_catalog."default" NOT NULL,
+    data_description character varying(100) COLLATE pg_catalog."default",
+    data_name character varying(25) COLLATE pg_catalog."default",
+    file_path character varying(255) COLLATE pg_catalog."default",
+    lr_lat numeric,
+    lr_lng numeric,
+    overlay_id integer NOT NULL,
+    resolution integer,
+    ul_lat numeric,
+    ul_lng numeric,
+    is_earth_daily boolean NOT NULL,
+    CONSTRAINT overlays_pkey PRIMARY KEY (overlay_id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.overlays
+    OWNER to azure_user;
